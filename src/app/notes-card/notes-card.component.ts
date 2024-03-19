d3c84daa2fe2d7982cd9360e45e5c335685629c6
Kamesh Kumar <kameshkumar4602@gmail.com>
2024-03-19 11:48:27 +0530

    Note Card Detailing

--- src/app/notes-card/notes-card.component.ts
+++ src/app/notes-card/notes-card.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild, AfterViewInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-notes-card',
   templateUrl: './notes-card.component.html',
   styleUrls: ['./notes-card.component.scss']
 })
-export class NotesCardComponent implements OnInit {
+export class NotesCardComponent implements AfterViewInit {
 
-  constructor() { }
+  @Input() title!:string;
+  @Input() body!:string;
 
-  ngOnInit(): void {
+  @ViewChild('truncator') truncator!: ElementRef<HTMLElement>;
+  @ViewChild('bodyText') bodyText!: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngAfterViewInit(): void {
+    const style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    const viewableHeight = parseInt(style.getPropertyValue('height'), 10);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
   }
-
 }
